ea46744fc721b758f383039731cfcc910c7e5a71
ahmadDev <ahmadjafari6050@gmail.com>
2024-09-01 13:35:52 +0330

    subscribe to auth service

--- apps/billing/src/billing.controller.ts
+++ apps/billing/src/billing.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject, OnModuleInit } from '@nestjs/common';
 import { BillingService } from './billing.service';
-import { EventPattern } from '@nestjs/microservices';
+import { ClientKafka, EventPattern } from '@nestjs/microservices';
 
 @Controller()
-export class BillingController {
-  constructor(private readonly billingService: BillingService) {}
+export class BillingController implements OnModuleInit {
+  constructor(
+    private readonly billingService: BillingService,
+    @Inject('AUTH_SERVICE') private readonly authClient: ClientKafka,
+  ) {}
 
   @Get()
   getHello(): string {
@@ -14,5 +17,9 @@ export class BillingController {
   @EventPattern('order_created')
   handleOrderCreated(data: any) {
     this.billingService.handleOrderCreated(data);
+  }
+
+  onModuleInit() {
+    this.authClient.subscribeToResponseOf('get_user');
   }
 }
